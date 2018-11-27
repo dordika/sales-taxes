add90f8a76df3d94b89fa0af7502ff0570e39467
dordika <dorjan.dika@gmai.com>
2018-11-27 23:25:41 +0100

    init project

---
+++ src/main/java/com/interview/salestaxes/SalesTaxesApplication.java
@@ -0,0 +1,12 @@
+package com.interview.salestaxes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesTaxesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalesTaxesApplication.class, args);
+    }
+}
